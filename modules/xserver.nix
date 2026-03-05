7533261be4992707dc75572f473010459d1a3480
Preston Pan <ret2pop@nullring.xyz>
2026-03-04 17:22:29 -0800

    cleanup garbage

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -6,21 +6,9 @@
       startx.enable = (! config.monorepo.profiles.ttyonly.enable);
     };
 
-    # windowManager = {
-	  #   i3 = {
-	  #     enable = (! config.monorepo.profiles.ttyonly.enable);
-	  #   };
-    # };
-
     desktopManager = {
 	    runXdgAutostartIfNone = true;
     };
-
-    # xkb = {
-	  #   layout = "us";
-	  #   variant = "";
-	  #   options = "caps:escape";
-    # };
 
     videoDrivers = (if config.monorepo.profiles.cuda.enable then [ "nvidia" ] else []);
   };
