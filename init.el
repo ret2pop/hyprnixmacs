53af771255dd5d6a9742c0e757a289313b04a612
Preston Pan <ret2pop@nullring.xyz>
2026-03-08 19:03:48 -0700

    maybe working again

--- init.el
+++ init.el
@@ -137,8 +137,7 @@
     (org-html-with-latex 'html)
     (org-html-mathjax-options nil)
     (org-html-mathjax-template "")
-    (org-latex-to-html-convert-command 
-      "printf '%%s' '%i' | pandoc -f latex -t html --mathml")
+    (org-latex-to-html-convert-command "pandoc -f latex -t html --mathml <<<%i")
     (org-html-viewport '((width "device-width") 
                          (initial-scale "1.0") 
                          (minimum-scale "1.0")) "Prevent zooming out past default size")
