acd671724d93675db85d68c670a53413bf95c773
Preston Pan <ret2pop@nullring.xyz>
2026-03-15 18:44:54 -0700

    fix init.el

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -4,16 +4,29 @@
   programs.git = {
     enable = lib.mkDefault config.monorepo.profiles.graphics.enable;
     package = pkgs.gitFull;
-    lfs.enable = lib.mkDefault config.monorepo.profiles.graphics.enable;
     userName = super.monorepo.vars.fullName;
-    userEmail = "${super.monorepo.vars.email}";
+    userEmail = super.monorepo.vars.email;
+    lfs.enable = lib.mkDefault config.monorepo.profiles.graphics.enable;
+
     signing = {
       key = super.monorepo.vars.gpgKey;
       signByDefault = true;
     };
 
+    # alias = {
+    #   pl = "pull";
+    #   ps = "push";
+    #   co = "checkout";
+    #   c = "commit";
+    #   a = "add";
+    #   st = "status";
+    #   sw = "switch";
+    #   b = "branch";
+    # };
+
     extraConfig = {
       init.defaultBranch = "main";
+
       credential."mail.${super.monorepo.vars.orgHost}" = {
         username = "${super.monorepo.vars.email}";
         helper = "!f() { test \"$1\" = get && echo \"password=$(cat /run/user/1000/secrets/mail)\"; }; f";
@@ -25,7 +38,16 @@
         smtpserverport = 465;
         smtpencryption = "ssl";
       };
+
     };
+    # settings = {
+    #   user = {
+    #     name = super.monorepo.vars.fullName;
+    #     email = super.monorepo.vars.email;
+    #   };
+
+      
+    # };
 
     aliases = {
       pl = "pull";
